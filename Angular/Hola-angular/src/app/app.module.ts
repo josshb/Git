a338c37549295614ba22bf99495f947c78dd464e
Josshb <mail@joshb.org>
2018-12-03 09:25:15 +0100

    fdsgsdgsdf

--- Angular/Hola-angular/src/app/app.module.ts
+++ Angular/Hola-angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlkdjfklgdfjklsComponent } from './slkdjfklgdfjkls/slkdjfklgdfjkls.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlkdjfklgdfjklsComponent,
+    AboutComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
